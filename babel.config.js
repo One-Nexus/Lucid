aa6ae305f17cf8a1da4e1bed3f44df014b126c7d
esr360 <esr360@live.com>
2019-12-15 01:09:07 +0000

    updating formatting

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 module.exports = function(api) {
-    api.cache(true);
+  api.cache(true);
 
-    return {
-        presets: [
-            '@babel/preset-env', 
-            '@babel/preset-react'
-        ],
-        plugins: [
-            '@babel/plugin-proposal-class-properties'
-        ]
-    };
+  return {
+    presets: [
+      '@babel/preset-env', 
+      '@babel/preset-react'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties'
+    ]
+  }
 }
